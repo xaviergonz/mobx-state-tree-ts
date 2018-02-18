8611b03730229b6501cd4b72ceb3131cc18b30a6
Javier Gonzalez <xaviergonz@gmail.com>
2018-02-18 13:58:37 +0100

    test typescript 2.8

--- src/utils.ts
+++ src/utils.ts
@@ -2,18 +2,8 @@ import { IArrayChange, IArraySplice, IKeyValueMap, IListenable, IMapEntries, IMa
 
 // tslint:disable:interface-over-type-literal
 
-export type DeepReadonly<T> = {
-  readonly [P in keyof T]: DeepReadonly<T[P]>;
-  };
-
 export type SinglePropertyObject<PropName extends string, PropType> = {
   [P in PropName]: PropType;
-  };
+};
 
-/**
- * Utilies types based on:
- * https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-307871458
- */
-export type Diff<T extends string, U extends string> = ({ [P in T]: P } &
-  { [P in U]: never } & { [x: string]: never })[T];
-export type Omit<T, K extends keyof T> = { [P in Diff<keyof T, K>]: T[P] };
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
